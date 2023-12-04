5d19929b409881ba8c25dc7d54f450319f3a74b1
vanapalli-kumar01 <130639003+vanapalli-kumar01@users.noreply.github.com>
2023-12-04 15:03:17 +0530

    Update Relocation.js

--- Relocation.js
+++ Relocation.js
@@ -5,7 +5,7 @@ async function postData(url = '', auth, data = {}) {
       mode: 'cors',
       headers: {
         'Authorization': auth,
-        'Content-Type': 'application/x-content-type-options',
+        'Content-Type': 'application/json',
       },
       body: JSON.stringify(data),
     });
@@ -13,22 +13,12 @@ async function postData(url = '', auth, data = {}) {
   };
   
   // method to sha256 encode string
-  async function sha256hex(message) {
+  async function sha256(message) {
     const msgBuffer = new TextEncoder('utf-8').encode(message);
     const hashBuffer = await crypto.subtle.digest('SHA-256', msgBuffer);
     const hashArray = Array.from(new Uint8Array(hashBuffer));
     return hashArray.map(b => ('00' + b.toString(16)).slice(-2)).join('');
   }
-function hash(string) {
-  // Create a SHA-256 hash object using the CryptoJS library
-  const hash = CryptoJS.SHA256(string);
-
-  // Convert the hash to a hexadecimal string
-  const hashedString = hash.toString(CryptoJS.enc.Hex);
-
-  return hashedString;
-}
-
   
   // Post data when form is submitted
   submitForm = async function() {
@@ -37,35 +27,40 @@ function hash(string) {
     const userId = queryParams.get('userId');
     const conversationId = queryParams.get('convId');
     const botId = queryParams.get('botId');
+//  const conversationId = "da251876-59eb-4bc5-8555-cf4626ef9dce";
+//      const botId = "59b84518-55ca-4d9f-bb9f-8ff5d8b2fe00";
+      
     // Get data from form
-    const employeeId = document.querySelector('input[name="employeeId"]').value;
+   const employeeId = document.querySelector('input[name="employeeId"]').value;
     const currentLocation = document.querySelector('select[name="currentLocation"]').value;
     const relocationTo = document.querySelector('select[name="relocationTo"]').value;
     const reasonForRelocation = document.querySelector('textarea[name="reasonForRelocation"]').value;
     const approver = document.querySelector('input[name="approver"]').value;
-    
-     // const password = document.getElementById('password').value;//
+
       console.log(employeeId);
     console.log(currentLocation);
     console.log(relocationTo);
     console.log(reasonForRelocation);
     console.log(approver);
+
+
   
     // use correct domain for your region
-    // const domain = 'https://va.bc-intg.liveperson.net/thirdparty-services-0.1/webview';
+    const domain = 'https://va.bc-intg.liveperson.net/thirdparty-services-0.1/webview';
     
-    // // encode auth string
-    // const authString = `${conversationId} || ${botId}`;
-    // const auth = await hash(authString);
+    // encode auth string
+    const authString = `${conversationId} || ${botId}`;
+    const auth = await sha256(authString);
   
-    // const res = await postData(domain, auth, {
-    //   botId,
-    //   conversationId,
-    //   userId,
-    //  message: "request successful", // optional
-    //   contextVariables: [
-    //     {"employeeId": "employeeId", "value": employeeId}
-    //   ],
-    // });
+    const res = await postData(domain, auth, {
+      botId,
+      conversationId,
+      userId,
+      message: "OverDueConv", // optional
+      contextVariables: [
+        
+          {"plainTextPassword1": "plainTextPassword", "value": "abc"}
+      ],
+    });
        window.close();     
   }
